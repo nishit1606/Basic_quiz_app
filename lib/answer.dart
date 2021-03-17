93fb23c0caab9d2996366f9e3ae89eb75a7fc05c
nishit1606 <nishit1606@gmail.com>
2021-03-17 23:32:42 +0530

    added more files

--- lib/answer.dart
+++ lib/answer.dart
@@ -10,6 +10,7 @@ class Answer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
+      // ignore: deprecated_member_use
       child: RaisedButton(
         color: Colors.blue,
         textColor: Colors.white,
