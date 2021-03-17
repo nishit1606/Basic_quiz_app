c9266b4486b442fbfb6fdaf26942b834c42b79c2
nishit1606 <nishit1606@gmail.com>
2021-03-17 19:25:50 +0530

    modified main and answer file

--- lib/answer.dart
+++ lib/answer.dart
@@ -2,15 +2,18 @@ import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
   final Function selectHandler;
-  Answer(this.selectHandler);
+  final String answerText;
+
+  Answer(this.selectHandler, this.answerText);
+
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      // ignore: deprecated_member_use
       child: RaisedButton(
         color: Colors.blue,
-        child: Text('Answer 1'),
+        textColor: Colors.white,
+        child: Text(answerText),
         onPressed: selectHandler,
       ),
     );
